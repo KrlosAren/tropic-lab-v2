f80c98ea56e72d4a86f38e7c8184c0156ed7e4ab
Carlos Lopez <krlosaren@gmail.com>
2021-01-25 10:07:59 -0300

    [add] project files and react

---
+++ src/routes/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../styles/index.css";
+
+const App = () => {
+  return (
+    <div className="main">
+      <h1>Tropic-lab</h1>
+      <p>Volveremos</p>
+    </div>
+  );
+};
+
+export default App;
